31b1dd3458d503a6ad9189a700fd59ace4b05e1b
Palchamy, Akshaya <akshaya@m-s.in>
2026-02-23 11:33:45 +0530

    ADDED: ToDoList Console application logics and test files.

---
+++ ToDoList.Test/TestToDoService.cs
@@ -0,0 +1,54 @@
+﻿using ToDoList.Data;
+
+namespace ToDoList.Test
+{
+    [TestClass]
+    public  class TestToDoService
+    {
+        ToDoService services = new ToDoService();
+
+        [TestMethod]
+        public void TestAddTask()
+        {
+           //Arrange
+           string Title = "Title";
+           string Description = "Description";
+           int Id = 1;
+
+           //Act
+           services.AddTask(Title);
+
+            //Assert
+            Assert.AreEqual("Title", Title);
+        }
+
+        [TestMethod]
+        public void TestViewTasks()
+        {
+            //Arrange
+            string Title = "Title";
+            string Description = "Description";
+            int Id = 1;
+
+            //Act
+            services.ViewTasks();
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestComplteTask()
+        {
+            //Arrange
+            string Title = "Title";
+            string Description = "Description";
+            int Id = 1;
+            services.AddTask("Title");
+
+            //Act
+            services.CompleteTask(1);
+
+            //Assert
+        }
+    }
+}
