219c336c4e7bc289d6f7b6a8ec69eb375e09966a
Akshaya <akshaya@astravor.com>
2026-02-23 11:35:40 +0530

    ADDED: PR- (#1)
    Co-authored-by: Palchamy, Akshaya <akshaya@m-s.in>

---
+++ ToDoList.Data/ToDoItem.cs
@@ -0,0 +1,22 @@
+﻿namespace ToDoList.Data
+{
+    //Encapsulation
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Title {  get; set; }
+        public bool IsCompleted { get; set; }
+
+        public ToDoItem(int id,string title) 
+        {
+            Id=id;
+            Title=title;
+            IsCompleted=false;
+        }
+
+        public void MarkCompleted()
+        {
+            IsCompleted=true;
+        }
+    }
+}
--- ToDoList.Terminal/ToDoList.Terminal/Program.cs
+++ ToDoList.Terminal/ToDoList.Terminal/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ToDoList.Common;
+using ToDoList.Data;
+
+namespace ToDoList.Terminal
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IToDoService service = new ToDoService();
+
+            while (true) 
+            {
+                Console.WriteLine("1.Add Task");
+                Console.WriteLine("2.View Task");
+                Console.WriteLine("3.Complete Task");
+                Console.WriteLine("4.Exit");
+
+                var Choice = Console.ReadLine();
+
+                switch (Choice) {
+                    case "1":
+                        Console.WriteLine("Enter Title");
+                        var title = Console.ReadLine();
+                        service.AddTask(title);
+                        break;
+                    case "2":
+                        service.ViewTasks();
+                        break;
+                    case "3":
+                        Console.WriteLine("Enter Task Id:");
+                        int id = int.Parse(Console.ReadLine());
+                        service.CompleteTask(id);
+                        break;
+                    case "4":
+                        return;
+                }
+            }
+        }
+    }
+}
